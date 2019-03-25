34197777dd5af0f01ac2dda078c26ae79ba3c610
LuuThom <luuthombnp95@gmail.com>
2019-03-26 03:05:01 +0700

    add data

---
+++ app/js/main.js
@@ -0,0 +1,57 @@
+// $(document).ready(function(){
+//   // $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
+//   new Chart(document.getElementById("doughnut-chart"), {
+//     type: 'doughnut',
+//     data: {
+//       labels: ['Returning', 'New'],
+//       datasets: [
+//         {
+//           data: [230, 130],
+//           backgroundColor: ['#1ca8dd', '#1bc98e'],
+//           // borderColor: '#252830',
+//         }
+//       ]
+//     },
+//     options: {
+//       responsive: true,
+//       legend: {
+//         position: 'top',
+//       },
+//       title: {
+//         display: true,
+//         // text: 'Chart.js Doughnut Chart'
+//       },
+//       animation: {
+//         animateScale: true,
+//         animateRotate: true
+//       }
+//     }
+//   });
+//   new Chart(document.getElementById("parkline1"), {
+//     type: 'line',
+//     data: {
+//       labels: ['a','b','c','d','e','f','g'],
+//       datasets: [
+//         {
+//           data: [28,68,41,43,96,45,100],
+//           backgroundColor: ['#1ca8dd', '#1bc98e'],
+//           borderColor: '#252830',
+//         }
+//       ]
+//     },
+//     options: {
+//       responsive: true,
+//       legend: {
+//         position: 'top',
+//       },
+//       title: {
+//         display: true,
+//         // text: 'Chart.js Doughnut Chart'
+//       },
+//       animation: {
+//         animateScale: true,
+//         animateRotate: true
+//       }
+//     }
+//   });
+// })
