e47bc8445444dffed2f3832a1360f44016eaf8a8
LuuThom <thom0702>
2019-03-26 17:05:24 +0700

    update-canvas

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,30 +1,47 @@
-function chart(element){
-  document.getElementById("#myChart") = element;
-}
-var myDoughnutChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    datasets: [{
-      data: [230, 130],
-      backgroundColor: ['#1ca8dd', '#1bc98e'],
-    }],
-    lable: {
-      'Returning', 'New'
-    }
-    options: {
-      responsive: true,
-      legend: {
-        position: 'top',
+$(document).ready(function () {
+
+  
+  // $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
+  function myChart(element) {
+    // function Chart(){
+    var el = $("#" + element);
+    // var el = document.getElementById(eleme =nt); 
+
+    var x = el.attr("data-dataset");
+    var t = JSON.parse(x);
+    var y = el.attr("data-lables");
+
+
+    var d = JSON.parse(y);
+
+    console.log(d)
+
+    new Chart(el, {
+      type: 'doughnut',
+      data: {
+        labels: y,
+        datasets: [
+          {
+            data: t,
+            backgroundColor: ['#1ca8dd', '#1bc98e'],
+            borderColor: '#252830',
+          }
+        ]
       },
-      title: {
-        display: true,
-        text: 'Chart.js Doughnut Chart'
-      },
-      animation: {
-        animateScale: true,
-        animateRotate: true
+      options: {
+        cutoutPercentage: 80,
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+        },
+        animation: {
+          animateScale: true,
+          animateRotate: true
+        }
       }
-    }
-  },
-  options: options
-});
+    })
+  }
+  myChart("doughnut-chart1");
--- app/js/main.js
+++ app/js/main.js
@@ -1,57 +1,77 @@
-// $(document).ready(function(){
-//   // $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
-//   new Chart(document.getElementById("doughnut-chart"), {
-//     type: 'doughnut',
-//     data: {
-//       labels: ['Returning', 'New'],
-//       datasets: [
-//         {
-//           data: [230, 130],
-//           backgroundColor: ['#1ca8dd', '#1bc98e'],
-//           // borderColor: '#252830',
-//         }
-//       ]
-//     },
-//     options: {
-//       responsive: true,
-//       legend: {
-//         position: 'top',
-//       },
-//       title: {
-//         display: true,
-//         // text: 'Chart.js Doughnut Chart'
-//       },
-//       animation: {
-//         animateScale: true,
-//         animateRotate: true
-//       }
-//     }
-//   });
-//   new Chart(document.getElementById("parkline1"), {
-//     type: 'line',
-//     data: {
-//       labels: ['a','b','c','d','e','f','g'],
-//       datasets: [
-//         {
-//           data: [28,68,41,43,96,45,100],
-//           backgroundColor: ['#1ca8dd', '#1bc98e'],
-//           borderColor: '#252830',
-//         }
-//       ]
-//     },
-//     options: {
-//       responsive: true,
-//       legend: {
-//         position: 'top',
-//       },
-//       title: {
-//         display: true,
-//         // text: 'Chart.js Doughnut Chart'
-//       },
-//       animation: {
-//         animateScale: true,
-//         animateRotate: true
-//       }
-//     }
-//   });
-// })
+$(document).ready(function(){
+  $(function () {
+    $('.js-form_datetime').eq(0).datepicker();
+    $(".js-canvas-chart").each(function () {
+      var $this = $(this),
+        _dataset = $this.attr("data-dataset"),
+        _labels = $this.attr("data-labels");
+      var datalist = JSON.parse(_dataset);
+
+      var arr = _labels.split(",");
+      new Chart($this, {
+        type: 'doughnut',
+        data: {
+          datasets: [
+            {
+              borderWidth: 2,
+              hoverBorderColor: "transparent",
+              borderColor: '#252830',
+              backgroundColor: ["#1ca8dd", "#1bc98e"],
+              data: datalist,
+            }
+          ],
+          labels: arr,
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          cutoutPercentage: 80,
+        }
+      });
+    });
+    $(".js-parkline1").each(function () {
+      // var ctx = document.getElementById("myChart");
+      var $this = $(this),
+        data = $this.data("dataset");
+      new Chart(data, {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              fill: !0,
+              backgroundColor: "rgba(255,255,255,.3)",
+              borderColor: "#fff",
+              pointBorderColor: "#fff",
+              lineTension: .25,
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              pointHitRadius: 20,
+              data: data,
+            }
+          ],
+          labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+        },
+        options: {
+          animation: {
+            duration: 0
+          },
+          legend: {
+            display: !1
+          },
+          scales: {
+            xAxes: [{
+              display: !1
+            }],
+            yAxes: [{
+              display: !1
+            }]
+          },
+          tooltips: {
+            enabled: !1
+          }
+        }
+      });
+    });
+  })
+})
